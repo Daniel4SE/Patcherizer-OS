b5ca2d2d922b91115b172154b7b2854c828acec3
Daniel4SE <xunzhu.tang@uni.lu>
2022-11-13 23:16:12 +0100

    update PatchCorrectness

--- PatchCorrectness/config_correctness.py
+++ PatchCorrectness/config_correctness.py
@@ -5,6 +5,6 @@ dirname = os.path.dirname(__file__)
 class Config:
     def __init__(self):
         # # original dataset with patches text and commit messages text.
-        # self.path_patch = '/Users/haoye.tian/Documents/ASE2022withTextUnique'
+        # self.path_patch = '/Users/xxx/Documents/ASE2022withTextUnique'
         # the feature from Tian et al.'s ASE2020 paper.
-        self.path_ASE2020_feature = '/Users/haoye.tian/Documents/University/data/ASE_features2_bert.pickle'
+        self.path_ASE2020_feature = '/Users/xxx/Documents/University/data/ASE_features2_bert.pickle'
